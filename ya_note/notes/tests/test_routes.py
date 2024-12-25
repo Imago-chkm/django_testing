ec1449fbba7ccfd75c6c074298a84c6419dd2751
Geychenko Mikhail <maikl-g95@yandex.ru>
2024-12-25 15:04:57 +0300

    убрал тест для главной страницы, сделал общий с путями для анонима

--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -1,13 +1,41 @@
 from http import HTTPStatus
 
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+
+from notes.models import Note
+
+User = get_user_model()
 
 
 class TestRouters(TestCase):
 
-    def test_home_page(self):
-        """Главная страница доступна анонимному пользователю."""
-        url = reverse('notes:home')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+    @classmethod
+    def setUpTestData(cls):
+        cls.note = Note.objects.create(
+            title='Заголовок',
+            text='Текст',
+            slug='slug',
+            author=cls.author
+        )
+        cls.author = User.objects.create(username='Лев Толстой')
+        cls.reader = User.objects.create(username='Читатель простой')
+
+    def test_availability_pages(self):
+        """Страницы доступные анонимному пользователю."""
+        urls = (
+            ('notes:home', None),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_notes_create_availability(self):
+        """Аутентифицированному пользователю доступны notes/, done/ и add/."""
+        

063c2c12e199be21935101698c7ea274471b96b4
Geychenko Mikhail <maikl-g95@yandex.ru>
2024-12-26 12:42:15 +0300

    добавлен тест на неповторение слаг

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -4,12 +4,15 @@ from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
 
+from notes.forms import WARNING
 from notes.models import Note
 
 User = get_user_model()
 
 
 class TestLogic(TestCase):
+
+    SLUG_TITLE = 'title'
 
     @classmethod
     def setUpTestData(cls):
@@ -25,7 +28,7 @@ class TestLogic(TestCase):
         cls.note = Note.objects.create(
             title='Заголовок',
             text='Текст',
-            slug='slug',
+            slug=cls.SLUG_TITLE,
             author=cls.author
         )
 
@@ -41,9 +44,18 @@ class TestLogic(TestCase):
         note_count = Note.objects.count()
         self.assertEqual(note_count, 2)
 
-    # def test_no_possible_two_same_slug(self):
-    #     """Невозможно создать две заметки с одинаковым slug."""
-    #     pass
+    def test_no_possible_two_same_slug(self):
+        """Невозможно создать две заметки с одинаковым slug."""
+        self.form_data.update({'slug': self.SLUG_TITLE})
+        response = self.author_client.post(self.url_add, data=self.form_data)
+        self.assertFormError(
+            response,
+            form='form',
+            field='slug',
+            errors=self.SLUG_TITLE + WARNING
+        )
+        note_count = Note.objects.count()
+        self.assertEqual(note_count, 1)
 
     # def test_automatic_slug_generation(self):
     #     """slug формируется автоматически, если не был заполнен."""

7f44b4fc42168224b57129412dcaa4a1b3a4c533
Geychenko Mikhail <maikl-g95@yandex.ru>
2024-12-27 17:53:29 +0300

    добавлен тест Страницы редактирования и удаления комментария доступны автору

--- ya_news/news/pytest_tests/conftest.py
+++ ya_news/news/pytest_tests/conftest.py
@@ -1,18 +1,59 @@
 import pytest
 
 from django.contrib.auth import get_user_model
+from django.test.client import Client
 
-from news.models import News
+from news.models import Comment, News
+
+User = get_user_model()
 
 
 @pytest.fixture
 def news():
     return News.objects.create(
         title='Заголовок',
-        text='Текст новости',
+        text='Текст',
     )
 
 
 @pytest.fixture
 def id_for_args(news):
     return news.id,
+
+
+@pytest.fixture
+def author(django_user_model):
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def comment(author, news):
+    return Comment.objects.create(
+        news=news,
+        text='Tекст',
+        author=author,
+    )
+
+
+@pytest.fixture
+def comment_id_for_args(comment):
+    return comment.id,
+
+
+@pytest.fixture
+def not_author(django_user_model):
+    return django_user_model.objects.create(username='Не автор')
+
+
+@pytest.fixture
+def author_client(author):
+    client = Client()
+    client.force_login(author)
+    return client
+
+
+@pytest.fixture
+def not_author_client(not_author):
+    client = Client()
+    client.force_login(not_author)
+    return client
--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -23,9 +23,32 @@ def test_pages_availability_for_anonymous_user(client, name, args):
     response = client.get(url)
     assert response.status_code == HTTPStatus.OK
 
-# def test_edit_delete_comments_availability_for_author():
-#     """Страницы редактирования и удаления комментария доступны автору."""
-#     pass
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
+        (lazy_fixture('author_client'), HTTPStatus.OK)
+    )
+)
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('news:edit', lazy_fixture('comment_id_for_args')),
+        ('news:delete', lazy_fixture('comment_id_for_args')),
+    ),
+)
+def test_edit_delete_comments_availability_for_author(
+    parametrized_client,
+    name,
+    args,
+    expected_status
+):
+    """Страницы редактирования и удаления комментария доступны автору."""
+    url = reverse(name, args=args)
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
 
 # def test_edit_delete_comments_redirect_for_anonim():
 #     """Редирект с редактирования и удаления комментария для анонима."""

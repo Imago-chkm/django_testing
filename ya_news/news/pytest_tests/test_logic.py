b046ec50481549dde9457c320ee7d841ca0f3ecd
Geychenko Mikhail <maikl-g95@yandex.ru>
2024-12-27 20:08:06 +0300

    тест на запрет писать стоп-слова

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -1,8 +1,11 @@
 import pytest
 
 from django.urls import reverse
+from pytest_django.asserts import assertFormError
 
+from news.forms import BAD_WORDS, WARNING
 from news.models import Comment
+
 
 
 @pytest.mark.django_db
@@ -25,9 +28,14 @@ def test_auth_user_can_create_comment(form_data, author_client, id_for_args):
     assert Comment.objects.count() == 1
 
 
-    # def test_user_cant_use_bad_words():
-    #     """Нельзя отправлять запрещенные слова."""
-    #     pass
+def test_user_cant_use_bad_words(author_client, id_for_args):
+    """Нельзя отправлять запрещенные слова."""
+    response = author_client.post(
+        reverse('news:detail', args=id_for_args),
+        data={'text': BAD_WORDS[0]}
+    )
+    assertFormError(response, 'form', 'text', errors=WARNING)
+    assert Comment.objects.count() == 0
 
     # def test_author_can_edit_his_comments(self):
     #     """Автор может редактировать свои комментарии."""

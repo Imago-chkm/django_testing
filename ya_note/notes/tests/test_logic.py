f09b8df3e9e6bcf2f45ac7d159001b9c82822071
Geychenko Mikhail <maikl-g95@yandex.ru>
2025-01-03 00:10:30 +0300

    правки в юнитест

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -11,15 +11,19 @@ class TestLogic(fixtures.Fixtures):
 
     def test_anonim_cant_create_note(self):
         """Аноним не может создавать заметки."""
+        Note.objects.all().delete()
+        expected_count = Note.objects.count()
         self.client.post(self.url_add, data=self.form_data)
         note_count = Note.objects.count()
-        self.assertEqual(note_count, 1)
+        self.assertEqual(note_count, expected_count)
 
     def test_logged_user_can_create_note(self):
         """Залогиненый юзер может создавать заметки."""
+        Note.objects.all().delete()
+        expected_count = Note.objects.count()
         self.author_client.post(self.url_add, data=self.form_data)
-        note_count = Note.objects.count()
-        self.assertEqual(note_count, 2)
+        note_count = Note.objects.count() - 1
+        self.assertEqual(note_count, expected_count)
 
     def test_no_possible_two_same_slug(self):
         """Невозможно создать две заметки с одинаковым slug."""
@@ -50,15 +54,18 @@ class TestLogic(fixtures.Fixtures):
             data=self.form_data
         )
         self.assertRedirects(response, self.url_success)
-        self.note.refresh_from_db()
-        self.assertEqual(self.note.text, self.NEW_NOTE_TEXT)
+        self.assertEqual(
+            Note.objects.get(id=self.note.id).text,
+            self.NEW_NOTE_TEXT
+        )
 
     def test_author_can_delete_his_notes(self):
         """Автор может удалять свои заметки."""
+        expected_count = Note.objects.count() - 1
         response = self.author_client.post(self.url_delete)
         self.assertRedirects(response, self.url_success)
         note_count = Note.objects.count()
-        self.assertEqual(note_count, 0)
+        self.assertEqual(note_count, expected_count)
 
     def test_user_cant_edit_another_users_notes(self):
         """Пользователь не может редактировать чужие заметки."""
@@ -67,12 +74,15 @@ class TestLogic(fixtures.Fixtures):
             data=self.form_data
         )
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
-        self.note.refresh_from_db()
-        self.assertEqual(self.note.text, self.NOTE_TEXT)
+        self.assertEqual(
+            Note.objects.get(id=self.note.id).text,
+            self.NOTE_TEXT
+        )
 
     def test_user_cant_delele_another_users_notes(self):
         """Пользователь не может удалять чужие заметки."""
+        expected_count = Note.objects.count()
         response = self.reader_client.post(self.url_delete)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
         note_count = Note.objects.count()
-        self.assertEqual(note_count, 1)
+        self.assertEqual(note_count, expected_count)
